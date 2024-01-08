99af57b872e406530677071ddb4e3a8cf6189991
LimpidCrypto <konermann@limpidcrypto.com>
2024-01-08 20:17:51 +0100

    add white background to logo

--- frontend/src/app/shared/components/atoms/image/image.component.ts
+++ frontend/src/app/shared/components/atoms/image/image.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'image-atom',
   templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss']
+  styleUrls: ['./image.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ImageComponent {
   @Input() src!: string;
